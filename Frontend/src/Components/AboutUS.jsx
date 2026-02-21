c5bc1151f0f140e7e27846d5096cf212089a98c5
scrxtch <peetr.svoboda@seznam.cz>
2026-02-21 21:22:15 +0100

    Nová db, praktická část

--- Frontend/src/Components/AboutUS.jsx
+++ Frontend/src/Components/AboutUS.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import Devider from "../pages/AboutUs/Devider";
 import Info from "../pages/AboutUs/Info";
 import History from "../pages/AboutUs/History";
 import Video from "../pages/AboutUs/Video";
@@ -8,7 +7,7 @@ import Review from "../pages/AboutUs/Review";
 function AboutUs() {
   return (
     <>
-      <Devider />
+      <div className="devider">náš oddíl</div>
       <Info />
       <div className="flex gap-5">
         <History />
