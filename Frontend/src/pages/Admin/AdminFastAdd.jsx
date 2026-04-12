74caafee68363bd40dd33714fef124a2fcf70b1b
JKtech <jktech@post.cz>
2026-04-12 14:51:27 +0200

    Fix paths

--- Frontend/src/pages/Admin/AdminFastAdd.jsx
+++ Frontend/src/pages/Admin/AdminFastAdd.jsx
@@ -7,20 +7,20 @@ function AdminFastAdd() {
           <div className="h-full flex items-center justify-between gap-2">
             <button className="flex w-full rounded-lg p-2 border-2 border-customBlack justify-evenly">
               <div className="bg-customGreen rounded-full w-6">
-                <img className="" src="../src/assets/Icons/plus.png" alt="Logo" />
+                <img className="" src="/assets/Icons/plus.png" alt="Logo" />
               </div>
               Přidat závodníka
             </button>
   
             <button className="flex w-full rounded-lg p-2 border-2 border-customBlack justify-evenly">
               <div className="bg-customGreen rounded-full w-6">
-                <img className="" src="../src/assets/Icons/plus.png" alt="Logo" />
+                <img className="" src="/assets/Icons/plus.png" alt="Logo" />
               </div>
               Přidat akci
             </button>
             <button className="flex w-full rounded-lg p-2 border-2 border-customBlack justify-evenly">
               <div className="bg-customGreen rounded-full w-6">
-                <img className="" src="../src/assets/Icons/plus.png" alt="Logo" />
+                <img className="" src="/assets/Icons/plus.png" alt="Logo" />
               </div>
               Přidat aktualitu
             </button>
