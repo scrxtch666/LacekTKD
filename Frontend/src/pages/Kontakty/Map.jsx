ff8363bdaaa8c61334c8400ab7d55779299599a7
scrxtch <peetr.svoboda@seznam.cz>
2026-04-14 20:23:18 +0200

    Env pro mapu

--- Frontend/src/pages/Kontakty/Map.jsx
+++ Frontend/src/pages/Kontakty/Map.jsx
@@ -2,7 +2,8 @@ import { useEffect, useRef } from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
-const API_KEY = '9MPPC5RXcG1E8weX-CVtMhwEWfe293q_60lCYt3KLVo';
+
+const API_KEY = import.meta.env.VITE_MAP_API;
 
 
 class LogoControl {
@@ -65,6 +66,7 @@ const Map = ({
       }
     };
   }, [latitude, longitude, zoom]);
+  console.log("API_KEY:", API_KEY);
 
   return (
     <div 
